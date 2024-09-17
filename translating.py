ca8c5ec706d81518bf9775d07bf93545a8aae927
Trawirr <gtrawirr@gmail.com>
2024-09-17 20:29:51 +0200

    script for creating word - translated word pairs using deepl

---
+++ translating.py
@@ -0,0 +1,17 @@
+import deepl
+
+auth_key = "..."
+translator = deepl.Translator(auth_key)
+
+with open("words.txt", "r") as f:
+    words = [word.strip() for word in f.readlines()]
+
+with open("translated", "w+", encoding="utf-8") as f:
+    for i, word in enumerate(words):
+        try:
+            print(f"{i}. {word} - ", end="")
+            translated = translator.translate_text(word, source_lang="EN", target_lang="PL").text
+            print(translated)
+            f.write(f"{word}-{translated}\n")
+        except Exception as e:
+            print(f"Exception: {e}")
